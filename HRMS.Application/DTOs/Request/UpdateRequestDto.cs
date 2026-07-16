68b21f8a0fdd49a95f2543e4d3c806e89b45799f
TheManofMedans <parhamkln980@gmail.com>
2026-07-16 19:01:59 +0330

    request updated

--- HRMS.Application/DTOs/Request/UpdateRequestDto.cs
+++ HRMS.Application/DTOs/Request/UpdateRequestDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRMS.domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,6 @@ namespace HRMS.Application.DTOs.Request
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public string? Description { get; set; }
+        public RequestStatus? Status { get; set; }
     }
 }
--- HRMS.Application/Services/RequestService.cs
+++ HRMS.Application/Services/RequestService.cs
@@ -43,5 +43,49 @@ namespace HRMS.Application.Services
             var requests = await _requestRepository.GetWithStatusAsync(status);
             return _mapper.Map<IEnumerable<RequestResponseDto>>(requests);
         }
+        public async Task<IEnumerable<RequestResponseDto>> GetWithTypeAsync(int type)
+        {
+            var requests = await _requestRepository.GetWithTypeAsync(type);
+            return _mapper.Map<IEnumerable<RequestResponseDto>>(requests); 
+        }
+        public async Task<IEnumerable<RequestResponseDto>> GetWithCustomDataAsync(int? EmployeeId, int? Status, int? Type)
+        {
+            var requests = await _requestRepository.GetWithCustomDataAsync(EmployeeId, Status, Type);
+            return _mapper.Map<IEnumerable<RequestResponseDto>>(requests);
+        }
+        public async Task<RequestResponseDto> CreateAsync(CreateRequestDto requestDto)
+        {
+            var request = _mapper.Map<Request>(requestDto);
+            var employee = await _employeeRepository.GetbyIdAsync(requestDto.EmployeeId);
+            request.EmployeeId = employee.Id;
+            await _requestRepository.AddAsync(request);
+            await _requestRepository.SaveChangesAsync();
+            return _mapper.Map<RequestResponseDto>(request);
+        }
+        public async Task<bool> UpdateAsync(int id,UpdateRequestDto requestDto)
+        {
+            var request = await _requestRepository.GetByIdAsync(id);
+            if (request is null)
+            {
+                return false;
+            }
+            request.ReviewedAt = requestDto.ReviewedAt;
+            request.Status = (domain.Enums.RequestStatus)requestDto.Status;
+            request.StartDate = (DateTime)requestDto.StartDate;
+            request.EndDate = (DateTime)requestDto.EndDate;
+            request.description = requestDto.Description;
+            _requestRepository.Update(request);
+            return await _requestRepository.SaveChangesAsync();
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var request = await _requestRepository.GetByIdAsync(id);
+            if (request is null)
+            {
+                return false;
+            }
+            _requestRepository.Delete(id);
+            return await _requestRepository.SaveChangesAsync();
+        }
     }
 }
