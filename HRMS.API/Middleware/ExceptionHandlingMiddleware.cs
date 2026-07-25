df89bdc87fa672d8eee979b603d16eccc9a36fe9
TheManofMedans <parhamkln980@gmail.com>
2026-07-25 18:07:38 +0330

    added Exception handling

---
+++ HRMS.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using HRMS.Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace HRMS.API.Middleware
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context,ex);
+            }
+
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var (StatusCode, title) = ex switch
+            {
+                NotFoundException => (HttpStatusCode.NotFound, "Resource Not Found"),
+                ConflictException => (HttpStatusCode.Conflict, "Conflict"),
+                RepeatDataException => (HttpStatusCode.Conflict,"Unauthorized Repeated Data"),
+                ValidationException => (HttpStatusCode.BadRequest,"Invalid Request"),
+                _ => (HttpStatusCode.InternalServerError,"An unexpected Error Occured!")
+            };
+            if (StatusCode == HttpStatusCode.InternalServerError)
+            {
+                _logger.LogError(ex, "Unhandled Exception on {Method} {path}", context.Request.Method, context.Request.Path);
+            }
+            else
+            {
+                _logger.LogInformation("{ExceptionType} {Message}",ex.GetType().Name,ex.Message);
+            }
+            var problemDetails = new ProblemDetails
+            {
+                Status = (int)StatusCode,
+                Title = title,
+                Detail = StatusCode == HttpStatusCode.InternalServerError ? "An unexpected error occurred. Please try again later." :
+                ex.Message,
+                Instance = context.Request.Path
+            };
+            context.Response.ContentType = "application/problem+json";
+            context.Response.StatusCode = problemDetails.Status.Value;
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
--- HRMS.API/Program.cs
+++ HRMS.API/Program.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using HRMS.Infrastructure.Persistence;
+using HRMS.API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<HRMSDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
