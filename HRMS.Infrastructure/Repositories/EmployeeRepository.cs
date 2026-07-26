aa095d7791f4c26b1d90537d9d65bbb7a6b0eecf
TheManofMedans <parhamkln980@gmail.com>
2026-07-26 15:20:57 +0330

    added actual employee repository

---
+++ HRMS.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRMS.Application.Interfaces.Repositories;
+using HRMS.domain.Entities;
+using HRMS.domain.Enums;
+using HRMS.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS.Infrastructure.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly HRMSDbContext _context;
+        public EmployeeRepository(HRMSDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Employee?> GetbyIdAsync(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+        public async Task<Employee?> GetByIdWithDepartmentsAsync(int id)
+        {
+            return await _context.Employees.
+                Include(e => e.EmployeeDepartments).
+                ThenInclude(ed => ed.Department).
+                FirstOrDefaultAsync(e => e.Id == id);
+        }
+        public async Task AddAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+        }
+        public void Update(Employee employee) 
+        {
+            _context.Employees.Update(employee);
+        }
+        public void Delete(Employee employee)
+        {
+            _context.Remove(employee);
+        }
+        public async Task<bool> EmailExistsAsync(string email) 
+        {
+            return await _context.Employees.AnyAsync(e => e.Email == email);
+        }
+        public async Task<bool> SSNExistsAsync(string SSN)
+        {
+            return await _context.Employees.AnyAsync(e => e.SSN == SSN);
+        }
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
