2ab711592a39cc775cd08fe9fa45749f97a67383
TheManofMedans <parhamkln980@gmail.com>
2026-07-29 14:22:35 +0330

    added Department and Employee Controller

---
+++ HRMS.API/Controllers/DepartmentController.cs
@@ -0,0 +1,55 @@
+﻿using HRMS.Application.Interfaces.Services;
+using HRMS.Application.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HRMS.Application.DTOs.Department;
+
+namespace HRMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var Departments = await _departmentService.GetAllAsync();
+            return Ok(Departments);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var department = await _departmentService.GetByIdAsync(id);
+            return department is null ? NotFound() : Ok(department);
+        }
+        [HttpGet("/Company/{Companyid}")]
+        public async Task<IActionResult> GetByCompanyId(int Companyid)
+        {
+            var departments = await _departmentService.GetByCompanyIdAsync(Companyid);
+            return Ok(departments);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateDepartmentDto dto)
+        {
+            var created = await _departmentService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById),new {id = created.Id},created);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateDepartmentDto dto)
+        {
+            await _departmentService.UpdateAsync(id,dto);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _departmentService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ HRMS.API/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HRMS.Application.DTOs;
+using HRMS.Application.Interfaces.Services;
+using HRMS.Application.DTOs.Employee;
+
+namespace HRMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var Employees = await _employeeService.GetAllAsync();
+            return Ok(Employees);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var Employee = await _employeeService.GetByIdAsync(id);
+            return Employee is null ? NotFound() : Ok(Employee);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateEmployeeDto dto)
+        {
+            var Created = await _employeeService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = Created.Id }, Created);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateEmployeeDto dto)
+        {
+            await _employeeService.UpdateAsync(id, dto);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _employeeService.DeleteAsync(id);
+            return NoContent();
+        }
+        [HttpPost("{Employeeid}/departments/{DepartmentId}/primary")]
+        public async Task<IActionResult> SetPrimary(int Employeeid,int Departmentid)
+        {
+            await _employeeService.SetPrimary(Employeeid, Departmentid);
+            return NoContent();
+        }
+        [HttpPost("{Employeeid}/department/{DepartmentId}")]
+        public async Task<IActionResult> AddToDepartmentAsync(int Employeeid,int Departmentid)
+        {
+            await _employeeService.AddToDepartmentAsync(Employeeid, Departmentid);
+            return NoContent();
+        }
+    }
+}
