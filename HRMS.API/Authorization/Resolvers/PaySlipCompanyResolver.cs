6ee40a7eb4cae2eefaf0b2ac3e7ec21ee83876a1
TheManofMedans <parhamkln980@gmail.com>
2026-09-12 14:48:59 +0330

    added resolvers

---
+++ HRMS.API/Authorization/Resolvers/PaySlipCompanyResolver.cs
@@ -0,0 +1,18 @@
+﻿using HRMS.Application.Interfaces.Repositories;
+
+namespace HRMS.API.Authorization.Resolvers
+{
+    public class PaySlipCompanyResolver : ICompanyResolver
+    {
+        private readonly IPaySlipRepository _paySlipRepository;
+        public PaySlipCompanyResolver(IPaySlipRepository paySlipRepository)
+        {
+            _paySlipRepository = paySlipRepository;
+        }
+        public async Task<int?> ResolveCompanyId(int resourceId)
+        {
+            var paySlip = await _paySlipRepository.GetByIdAsync(resourceId);
+            return paySlip?.Department?.CompanyId;
+        }
+    }
+}
---
+++ HRMS.API/Authorization/Resolvers/PaySlipOwnerResolver.cs
@@ -0,0 +1,18 @@
+﻿using HRMS.Application.Interfaces.Repositories;
+
+namespace HRMS.API.Authorization.Resolvers
+{
+    public class PaySlipOwnerResolver : IResourceOwnerResolver
+    {
+        private readonly IPaySlipRepository _paySlipRepository;
+        public PaySlipOwnerResolver(IPaySlipRepository paySlipRepository)
+        {
+            _paySlipRepository = paySlipRepository;
+        }
+        public async Task<int?> ResolveEmployeeId(int resourceId)
+        {
+            var paySlip = await _paySlipRepository.GetByIdAsync(resourceId);
+            return paySlip?.EmployeeId;
+        }
+    }
+}
